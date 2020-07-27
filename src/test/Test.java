5867ff7edd0ed7d707c4ac62e81d63d4465db199
vigilr <3496655347@qq.com>
2020-07-27 23:45:53 +0800

    添加测试环境

---
+++ src/test/Test.java
@@ -0,0 +1,14 @@
+package test;
+
+/**
+ * @author Vigilr
+ * @since 2020-07-27
+ */
+public class Test {
+    public static void main(String[] args) {
+        String s="hello";
+        for (int i=0;i<s.length();i++){
+            System.out.println(s.charAt(i));
+        }
+    }
+}
