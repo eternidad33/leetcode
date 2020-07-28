8e71567c1a73f46c7e6fac3e17220b686587aea7
vigilr <3496655347@qq.com>
2020-07-28 16:11:42 +0800

    1 ms	39.6 MB

--- editor/[104]二叉树的最大深度.java
+++ editor/[104]二叉树的最大深度.java
@@ -32,7 +32,30 @@
 class Solution {
     public int maxDepth(TreeNode root) {
         //递归方法
-        return root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+//        return root == null ? 0 : Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+        //广度优先搜索
+        if (root == null) {
+            return 0;
+        } else {
+            Queue<TreeNode> queue = new LinkedList<>();
+            queue.offer(root);
+            int ans = 0;
+            while (!queue.isEmpty()) {
+                int size = queue.size();
+                while (size > 0) {
+                    TreeNode node = queue.remove();
+                    if (node.left != null) {
+                        queue.offer(node.left);
+                    }
+                    if (node.right != null) {
+                        queue.offer(node.right);
+                    }
+                    size--;
+                }
+                ans++;
+            }
+            return ans;
+        }
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
