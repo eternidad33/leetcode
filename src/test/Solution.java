c145ddd989eef4275f3665323e056d5aa47c43cc
vigilr <3496655347@qq.com>
2020-07-28 16:37:31 +0800

    3 ms	41.4 MB

--- src/test/Solution.java
+++ src/test/Solution.java
@@ -34,6 +34,26 @@ public class Solution {
             }
             return ans;
         }
+
+    }
+
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int count = 0, max = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+                count++;
+            } else {
+                max = Math.max(count, max);
+                count = 0;
+            }
+        }
+        return max;
+    }
+
+    public static void main(String[] args) {
+        int[] nums=new int[]{1};
+        Solution solution=new Solution();
+        System.out.println(solution.findMaxConsecutiveOnes(nums));
     }
 }
 
