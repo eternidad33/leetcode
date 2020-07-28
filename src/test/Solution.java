96842856b7f9d0ae4a98146c0b6b25988f59f652
vigilr <3496655347@qq.com>
2020-07-28 18:12:13 +0800

    3 ms	38.3 MB

--- src/test/Solution.java
+++ src/test/Solution.java
@@ -6,51 +6,11 @@ package test;
  */
 public class Solution {
 
-    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
-        ListNode dummyHead = new ListNode(0);
-        ListNode p = l1, q = l2, curr = dummyHead;
-        int carry = 0;
-        while (p != null || q != null) {
-            int x = (p != null) ? p.val : 0;
-            int y = (q != null) ? q.val : 0;
-            int sum = carry + x + y;
-            carry = sum / 10;
-            curr.next = new ListNode(sum % 10);
-            curr = curr.next;
-            if (p != null) {
-                p = p.next;
-            }
-            if (q != null) {
-                q = q.next;
-            }
-        }
-        if (carry > 0) {
-            curr.next = new ListNode(carry);
-        }
-        return dummyHead.next;
+    public String addBinary(String a, String b) {
+        return Integer.toBinaryString(Integer.parseInt(a, 2) + Integer.parseInt(b, 2));
     }
-
 
     public static void main(String[] args) {
-        Solution solution = new Solution();
-        ListNode l1 = new ListNode(2);
-        l1.next = new ListNode(4);
-        l1.next.next = new ListNode(3);
-        ListNode l2 = new ListNode(5);
-        l2.next = new ListNode(6);
-        l2.next.next = new ListNode(4);
-        ListNode l3 = solution.addTwoNumbers(l1, l2);
-        System.out.println(l3.val);
-        System.out.println(l3.next.val);
-        System.out.println(l3.next.next.val);
-    }
-}
 
-class ListNode {
-    int val;
-    ListNode next;
-
-    ListNode(int x) {
-        val = x;
     }
 }
