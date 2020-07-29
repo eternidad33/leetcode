240e89f78c15915e1d4a20498a3f29f5318c167b
vigilr <3496655347@qq.com>
2020-07-29 16:18:07 +0800

    6 ms	42.5 MB

--- editor/[56]合并区间.java
+++ editor/[56]合并区间.java
@@ -1,14 +1,23 @@
-package test;
+//给出一个区间的集合，请合并所有重叠的区间。 
+//
+// 示例 1: 
+//
+// 输入: [[1,3],[2,6],[8,10],[15,18]]
+//输出: [[1,6],[8,10],[15,18]]
+//解释: 区间 [1,3] 和 [2,6] 重叠, 将它们合并为 [1,6].
+// 
+//
+// 示例 2: 
+//
+// 输入: [[1,4],[4,5]]
+//输出: [[1,5]]
+//解释: 区间 [1,4] 和 [4,5] 可被视为重叠区间。 
+// Related Topics 排序 数组 
+// 👍 525 👎 0
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
-/**
- * @author Vigilr
- * @since 2020-07-28
- */
-public class Solution {
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
     public int[][] merge(int[][] intervals) {
         List<int[]> result = new ArrayList<>();
         if (intervals.length == 0) {
@@ -29,8 +38,5 @@ public class Solution {
         }
         return result.toArray(new int[0][]);
     }
-
-    public static void main(String[] args) {
-
-    }
 }
+//leetcode submit region end(Prohibit modification and deletion)
