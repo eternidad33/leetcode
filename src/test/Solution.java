28b32192774264b7b060473d5a51c635967607d1
vigilr <3496655347@qq.com>
2020-07-29 15:23:53 +0800

    1 ms	40.8 MB

--- src/test/Solution.java
+++ src/test/Solution.java
@@ -5,12 +5,27 @@ package test;
  * @since 2020-07-28
  */
 public class Solution {
-
-    public String addBinary(String a, String b) {
-        return Integer.toBinaryString(Integer.parseInt(a, 2) + Integer.parseInt(b, 2));
+    public int pivotIndex(int[] nums) {
+        int n = nums.length;
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+            sum += nums[i];
+        }
+        int s = 0;
+        for (int i = 0; i < n; i++) {
+            if (s == sum - s - nums[i]) {
+                return i;
+            } else {
+                s += nums[i];
+            }
+        }
+        return -1;
     }
 
     public static void main(String[] args) {
-
+        int[] nums = new int[]{-1, -1, -1, -1, -1, -1};
+        Solution solution = new Solution();
+        int s = solution.pivotIndex(nums);
+        System.out.println(s);
     }
 }
