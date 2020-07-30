46a68f58e314d7f4516e4f2a8dc933a56644a6d3
vigilr <3496655347@qq.com>
2020-07-30 09:47:02 +0800

    4 ms	40.1 MB

--- editor/[167]两数之和 II - 输入有序数组.java
+++ editor/[167]两数之和 II - 输入有序数组.java
@@ -21,15 +21,20 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public int[] twoSum(int[] numbers, int target) {
-        int n = numbers.length;
-        for (int i = 0; i < n - 1; i++) {
-            for (int j = i + 1; j < n; j++) {
-                if (numbers[i] + numbers[j] == target) {
-                    return new int[]{i+1,j+1};
+        for (int i = 0; i < numbers.length; i++) {
+            int left = i + 1, right = numbers.length - 1;
+            while (left <= right) {
+                int mid = (right - left) / 2 + left;
+                if (numbers[mid] == target - numbers[i]) {
+                    return new int[]{i + 1, mid + 1};
+                } else if (numbers[mid] > target - numbers[i]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
                 }
             }
         }
-        return new int[]{-1,-1};
+        return new int[]{-1, -1};
     }
 }
 //leetcode submit region end(Prohibit modification and deletion)
