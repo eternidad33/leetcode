dbf07c9a85eb82cd356d59766f12ed5b46a98578
vigilr <3496655347@qq.com>
2020-08-01 17:24:01 +0800

    12 ms	41.7 MB

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -1,5 +1,6 @@
 package main;
 
+import java.util.Arrays;
 import java.util.TreeSet;
 
 /**
@@ -19,6 +20,12 @@ public class Solution {
             }
         }
         return set.size() < 3 ? set.last() : set.first();
+    }
+
+    public int maximumProduct(int[] nums) {
+        Arrays.sort(nums);
+        int n = nums.length;
+        return Math.max(nums[0] * nums[1] * nums[2], nums[n - 3] * nums[n - 2] * nums[n - 1]);
     }
 
     public static void main(String[] args) {
