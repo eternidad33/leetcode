45dcef0afa95d72fce9df2826096a13fc1329a39
vigilr <3496655347@qq.com>
2020-08-02 14:22:51 +0800

    2 ms	41.8 MB

---
+++ 数组/[645]错误的集合.java
@@ -0,0 +1,41 @@
+//集合 S 包含从1到 n 的整数。不幸的是，因为数据错误，导致集合里面某一个元素复制了成了集合里面的另外一个元素的值，导致集合丢失了一个整数并且有一个元素重
+//复。 
+//
+// 给定一个数组 nums 代表了集合 S 发生错误后的结果。你的任务是首先寻找到重复出现的整数，再找到丢失的整数，将它们以数组的形式返回。 
+//
+// 示例 1: 
+//
+// 
+//输入: nums = [1,2,2,4]
+//输出: [2,3]
+// 
+//
+// 注意: 
+//
+// 
+// 给定数组的长度范围是 [2, 10000]。 
+// 给定的数组是无序的。 
+// 
+// Related Topics 哈希表 数学 
+// 👍 102 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int[] counter = new int[nums.length + 1];
+        for (int num : nums) {
+            counter[num]++;
+        }
+        int[] result = new int[2];
+        for (int i = 1; i < counter.length; i++) {
+            if (counter[i] == 0) {
+                result[1] = i;
+            } else if (counter[i] == 2) {
+                result[0] = i;
+            }
+        }
+        return result;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
