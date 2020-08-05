807d2b263c05205920e1fa4d9a732ce7905df00e
vigilr <3496655347@qq.com>
2020-08-05 10:33:04 +0800

    60ms,13.6MB

--- Python solution/41.缺失的第一个正数.py
+++ Python solution/41.缺失的第一个正数.py
@@ -9,18 +9,11 @@ class Solution:
     def firstMissingPositive(self, nums: List[int]) -> int:
         n = len(nums)
         for i in range(n):
-            if nums[i] <= 0:
-                nums[i] = n + 1
-        
+            while 1 <= nums[i] <= n and nums[nums[i] - 1] != nums[i]:
+                nums[nums[i] - 1], nums[i] = nums[i], nums[nums[i] - 1]
         for i in range(n):
-            num = abs(nums[i])
-            if num <= n:
-                nums[num - 1] = -abs(nums[num - 1])
-        
-        for i in range(n):
-            if nums[i] > 0:
+            if nums[i] != i + 1:
                 return i + 1
-        
         return n + 1
 # @lc code=end
 
