40db0aed93dcb8d577afe9abdb50205bccdfa035
vigilr <3496655347@qq.com>
2020-08-05 09:41:24 +0800

    7 ms 48.8 MB

--- src/main/Solution.java
+++ src/main/Solution.java
@@ -7,40 +7,18 @@ import java.util.*;
  * @since 2020-07-28
  */
 public class Solution {
-    public String addStrings(String num1, String num2) {
-        int i = num1.length() - 1, j = num2.length() - 1, add = 0;
-        StringBuffer stringBuffer = new StringBuffer();
-        while (i >= 0 || j >= 0 || add != 0) {
-            int x = i >= 0 ? num1.charAt(i) - '0' : 0;
-            int y = j >= 0 ? num2.charAt(j) - '0' : 0;
-            int result = x + y + add;
-            stringBuffer.append(result % 10);
-            add = result / 10;
-            i--;
-            j--;
-        }
-        return stringBuffer.reverse().toString();
-    }
-
-    public List<Integer> findDisappearedNumbers(int[] nums) {
+    public List<Integer> findDuplicates(int[] nums) {
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < nums.length; i++) {
-            int newIndex = Math.abs(nums[i]) - 1;
-            if (nums[newIndex] > 0) {
-                nums[newIndex] *= -1;
-            }
-        }
-        List<Integer> list = new LinkedList<>();
-        for (int i = 0; i < nums.length; i++) {
-            if (nums[i] > 0) {
-                list.add(i + 1);
-            }
+            int index = Math.abs(nums[i]) - 1;
+            if (nums[index] < 0)
+                list.add(Math.abs(index + 1));
+            nums[index] = -nums[index];
         }
         return list;
     }
 
     public static void main(String[] args) {
         Solution solution = new Solution();
-        String s = solution.addStrings("123", "459");
-        System.out.println(s);
     }
 }
