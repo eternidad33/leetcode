81078b5aa0b7ce23b29ff2c2c8deb131433b42d1
vigilr <3496655347@qq.com>
2020-09-23 22:47:40 +0800

    双百 64ms

--- 【LeetBook】数组和字符串/[5]最长回文子串.py
+++ 【LeetBook】数组和字符串/[5]最长回文子串.py
@@ -23,25 +23,45 @@ class Solution(object):
         :type s: str
         :rtype: str
         """
-        # 动态规划
-        n = len(s)
-        dp = [[False] * n for _ in range(n)]
-        ans = ""
-        # 枚举子串的长度 length+1
-        for length in range(n):
-            # 枚举子串的起始位置 i，这样可以通过 j=i+length 得到子串的结束位置
-            for i in range(n):
-                j = i + length
-                if j >= len(s):
-                    break
-                if length == 0:
-                    dp[i][j] = True
-                elif length == 1:
-                    dp[i][j] = (s[i] == s[j])
-                else:
-                    dp[i][j] = (dp[i + 1][j - 1] and s[i] == s[j])
-                if dp[i][j] and length + 1 > len(ans):
-                    ans = s[i:j + 1]
-        return ans
+
+        if not s:
+            return ""
+        length = len(s)
+        if length == 1 or s == s[::-1]:
+            return s
+        max_len, start = 1, 0
+        for i in range(1, length):
+            even = s[i - max_len:i + 1]
+            odd = s[i - max_len - 1:i + 1]
+            if i - max_len - 1 >= 0 and odd == odd[::-1]:
+                start = i - max_len - 1
+                max_len += 2
+                continue
+            if i - max_len >= 0 and even == even[::-1]:
+                start = i - max_len
+                max_len += 1
+                continue
+        return s[start:start + max_len]
+
+        # # 动态规划
+        # n = len(s)
+        # dp = [[False] * n for _ in range(n)]
+        # ans = ""
+        # # 枚举子串的长度 length+1
+        # for length in range(n):
+        #     # 枚举子串的起始位置 i，这样可以通过 j=i+length 得到子串的结束位置
+        #     for i in range(n):
+        #         j = i + length
+        #         if j >= len(s):
+        #             break
+        #         if length == 0:
+        #             dp[i][j] = True
+        #         elif length == 1:
+        #             dp[i][j] = (s[i] == s[j])
+        #         else:
+        #             dp[i][j] = (dp[i + 1][j - 1] and s[i] == s[j])
+        #         if dp[i][j] and length + 1 > len(ans):
+        #             ans = s[i:j + 1]
+        # return ans
 
 # leetcode submit region end(Prohibit modification and deletion)
