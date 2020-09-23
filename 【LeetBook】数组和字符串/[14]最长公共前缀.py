55a1d9671b4a852ad659142d4b7b74b559ed39fc
vigilr <3496655347@qq.com>
2020-09-23 22:24:22 +0800

    52 ms

---
+++ 【LeetBook】数组和字符串/[14]最长公共前缀.py
@@ -0,0 +1,48 @@
+# 编写一个函数来查找字符串数组中的最长公共前缀。 
+# 
+#  如果不存在公共前缀，返回空字符串 ""。 
+# 
+#  示例 1: 
+# 
+#  输入: ["flower","flow","flight"]
+# 输出: "fl"
+#  
+# 
+#  示例 2: 
+# 
+#  输入: ["dog","racecar","car"]
+# 输出: ""
+# 解释: 输入不存在公共前缀。
+#  
+# 
+#  说明: 
+# 
+#  所有输入只包含小写字母 a-z 。 
+#  Related Topics 字符串 
+#  👍 1280 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+class Solution(object):
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if not strs:
+            return ""
+
+        prefix, count = strs[0], len(strs)
+        for i in range(1, count):
+            prefix = self.lcp(prefix, strs[i])
+            if not prefix:
+                break
+
+        return prefix
+
+    def lcp(self, str1, str2):
+        length, index = min(len(str1), len(str2)), 0
+        while index < length and str1[index] == str2[index]:
+            index += 1
+        return str1[:index]
+# leetcode submit region end(Prohibit modification and deletion)
