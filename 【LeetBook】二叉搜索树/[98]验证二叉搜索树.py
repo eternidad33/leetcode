79a3934b7a19395ac104919b5fcc4e18950c28dd
vigilr <3496655347@qq.com>
2020-10-26 22:26:58 +0800

    72ms

---
+++ 【LeetBook】二叉搜索树/[98]验证二叉搜索树.py
@@ -0,0 +1,65 @@
+# 给定一个二叉树，判断其是否是一个有效的二叉搜索树。 
+# 
+#  假设一个二叉搜索树具有如下特征： 
+# 
+#  
+#  节点的左子树只包含小于当前节点的数。 
+#  节点的右子树只包含大于当前节点的数。 
+#  所有左子树和右子树自身必须也是二叉搜索树。 
+#  
+# 
+#  示例 1: 
+# 
+#  输入:
+#     2
+#    / \
+#   1   3
+# 输出: true
+#  
+# 
+#  示例 2: 
+# 
+#  输入:
+#     5
+#    / \
+#   1   4
+#      / \
+#     3   6
+# 输出: false
+# 解释: 输入为: [5,1,4,null,null,3,6]。
+#      根节点的值为 5 ，但是其右子节点值为 4 。
+#  
+#  Related Topics 树 深度优先搜索 
+#  👍 817 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+# Definition for a binary tree node.
+class TreeNode:
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+
+
+class Solution:
+    def isValidBST(self, root: TreeNode) -> bool:
+        def is_sorted(root: TreeNode, lower=float('-inf'), upper=float('inf')):
+            """上界和下界"""
+            if not root:
+                return True
+
+            val = root.val
+            if val <= lower or val >= upper:
+                return False
+
+            if not is_sorted(root.left, lower, val):
+                return False
+            if not is_sorted(root.right, val, upper):
+                return False
+
+            return True
+
+        return is_sorted(root)
+
+# leetcode submit region end(Prohibit modification and deletion)
