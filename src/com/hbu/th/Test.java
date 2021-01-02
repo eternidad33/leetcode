140d38aeb0eaeed097d0d98a3ee3392a3b6ba9ed
v <3496655347@qq.com>
2021-01-02 22:10:52 +0800

    不完善的基于Socket的对弈幻方

---
+++ src/com/hbu/th/Test.java
@@ -0,0 +1,13 @@
+package com.hbu.th;
+
+/**
+ * @author vigilr
+ * @since 2020/12/31
+ */
+public class Test {
+    public static void main(String[] args) {
+        int a=0;
+        String b=Integer.toString(a);
+        System.out.println(b);
+    }
+}
