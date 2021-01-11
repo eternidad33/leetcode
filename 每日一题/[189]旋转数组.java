a5d1b85b07a1dc2a76b445164b2dd20137686a5a
v <3496655347@qq.com>
2021-01-11 18:03:39 +0800

    0ms 翻转数组

--- 每日一题/[189]旋转数组.java
+++ 每日一题/[189]旋转数组.java
@@ -31,14 +31,29 @@
 //leetcode submit region begin(Prohibit modification and deletion)
 class Solution {
     public void rotate(int[] nums, int k) {
-        //开辟新的数组
-        int n = nums.length;
-        int[] newArray = new int[n];
-        for (int i = 0; i < n; i++) {
-            newArray[(i + k) % n] = nums[i];
-        }
-        for (int j = 0; j < n; j++) {
-            nums[j] = newArray[j];
+//        //开辟新的数组
+//        int n = nums.length;
+//        int[] newArray = new int[n];
+//        for (int i = 0; i < n; i++) {
+//            newArray[(i + k) % n] = nums[i];
+//        }
+//        for (int j = 0; j < n; j++) {
+//            nums[j] = newArray[j];
+//        }
+
+        //翻转数组法，先全部翻转，在分别翻转前半部分和后半部分
+        //如果k大于数组长度，变成余数
+        k %= nums.length;
+        reverse(nums, 0, nums.length - 1);
+        reverse(nums, 0, k - 1);
+        reverse(nums, k, nums.length - 1);
+    }
+
+    public void reverse(int[] nums, int start, int end) {
+        while (start < end) {
+            int temp = nums[start];
+            nums[start++] = nums[end];
+            nums[end--] = temp;
         }
     }
 }
