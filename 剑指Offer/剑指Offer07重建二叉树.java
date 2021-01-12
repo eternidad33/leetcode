650036ab97ef959abdc6deb90f1ce6832eb18c76
v <3496655347@qq.com>
2021-01-12 23:21:08 +0800

    2ms 递归

---
+++ 剑指Offer/剑指Offer07重建二叉树.java
@@ -0,0 +1,71 @@
+//输入某二叉树的前序遍历和中序遍历的结果，请重建该二叉树。假设输入的前序遍历和中序遍历的结果中都不含重复的数字。 
+//
+// 
+//
+// 例如，给出 
+//
+// 前序遍历 preorder = [3,9,20,15,7]
+//中序遍历 inorder = [9,3,15,20,7] 
+//
+// 返回如下的二叉树： 
+//
+//     3
+//   / \
+//  9  20
+//    /  \
+//   15   7 
+//
+// 
+//
+// 限制： 
+//
+// 0 <= 节点个数 <= 5000 
+//
+// 
+//
+// 注意：本题与主站 105 题重复：https://leetcode-cn.com/problems/construct-binary-tree-from-
+//preorder-and-inorder-traversal/ 
+// Related Topics 树 递归 
+// 👍 292 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+
+/**
+ * Definition for a binary tree node. public class TreeNode { int val; TreeNode left; TreeNode right; TreeNode(int x) {
+ * val = x; } }
+ */
+class Solution {
+    public TreeNode buildTree(int[] preorder, int[] inorder) {
+        if (preorder == null || preorder.length == 0) {
+            return null;
+        }
+        Map<Integer, Integer> indexMap = new HashMap<Integer, Integer>();
+        int length = preorder.length;
+        for (int i = 0; i < length; i++) {
+            indexMap.put(inorder[i], i);
+        }
+        TreeNode root = buildTree(preorder, 0, length - 1, inorder, 0, length - 1, indexMap);
+        return root;
+    }
+
+    public TreeNode buildTree(int[] preorder, int preorderStart, int preorderEnd, int[] inorder, int inorderStart, int inorderEnd, Map<Integer, Integer> indexMap) {
+        if (preorderStart > preorderEnd) {
+            return null;
+        }
+        int rootVal = preorder[preorderStart];
+        TreeNode root = new TreeNode(rootVal);
+        if (preorderStart == preorderEnd) {
+            return root;
+        } else {
+            int rootIndex = indexMap.get(rootVal);
+            int leftNodes = rootIndex - inorderStart, rightNodes = inorderEnd - rootIndex;
+            TreeNode leftSubtree = buildTree(preorder, preorderStart + 1, preorderStart + leftNodes, inorder, inorderStart, rootIndex - 1, indexMap);
+            TreeNode rightSubtree = buildTree(preorder, preorderEnd - rightNodes + 1, preorderEnd, inorder, rootIndex + 1, inorderEnd, indexMap);
+            root.left = leftSubtree;
+            root.right = rightSubtree;
+            return root;
+        }
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
