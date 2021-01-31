e08bab8308e9ce3536eeec1ac18b00095c881306
v <3496655347@qq.com>
2021-01-31 09:57:22 +0800

    ac

--- src/com/hbu/contest/满足三条件之一需改变的最少字符数.java
+++ src/com/hbu/contest/满足三条件之一需改变的最少字符数.java
@@ -6,22 +6,26 @@ package com.hbu.contest;
  */
 public class 满足三条件之一需改变的最少字符数 {
     public int minCharacters(String a, String b) {
-        char minA = 'z', minB = 'z';
-        int countA = 1, countB = 1;
-        findMinCount(a, minA, countA);
-        findMinCount(b, minB, countB);
-        return 0;
-    }
+        int[] acnt = new int[26];
+        int[] bcnt = new int[26];
+        int an = a.length(), bn = b.length();
 
-    public void findMinCount(String s, char min, int count) {
-        for (int i = 0; i < s.length(); i++) {
-            if (s.charAt(i) < min) {
-                min = s.charAt(i);
-                count = 1;
-            }
-            if (s.charAt(i) == min) {
-                count++;
-            }
+        for (int i = 0; i < an; i++) {
+            char c = a.charAt(i);
+            acnt[c - 'a']++;
         }
+        for (int i = 0; i < bn; i++) {
+            char c = b.charAt(i);
+            bcnt[c - 'a']++;
+        }
+        int ans = Integer.MAX_VALUE, asum = 0, bsum = 0;
+        for (int i = 0; i < 25; i++) {
+            asum += acnt[i];
+            bsum += bcnt[i];
+            ans = Math.min(Math.min(ans, an - acnt[i] + bn - bcnt[i]), Math.min(an - asum + bsum, bn - bsum + asum));
+        }
+        ans = Math.min(ans, an - acnt[25] + bn - bcnt[25]);
+
+        return ans;
     }
 }
