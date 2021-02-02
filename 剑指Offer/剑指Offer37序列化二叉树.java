cc5c2802508389ebba18b90bfa0396155d694899
v <3496655347@qq.com>
2021-02-02 23:14:41 +0800

    24ms 层序遍历

---
+++ 剑指Offer/剑指Offer37序列化二叉树.java
@@ -0,0 +1,92 @@
+//请实现两个函数，分别用来序列化和反序列化二叉树。 
+//
+// 示例: 
+//
+// 你可以将以下二叉树：
+//
+//    1
+//   / \
+//  2   3
+//     / \
+//    4   5
+//
+//序列化为 "[1,2,3,null,null,4,5]" 
+//
+// 注意：本题与主站 297 题相同：https://leetcode-cn.com/problems/serialize-and-deserialize-b
+//inary-tree/ 
+// Related Topics 树 设计 
+// 👍 104 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+public class Codec {
+
+    // Encodes a tree to a single string.
+    public String serialize(TreeNode root) {
+        //树为空，直接返回null
+        if (root == null) {
+            return "[]";
+        }
+        StringBuilder res = new StringBuilder("[");
+        //队列初始化
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        //层序遍历
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (node != null) {
+                res.append(node.val + ",");
+                queue.add(node.left);
+                queue.add(node.right);
+            } else {
+                //节点为null时，结果+null
+                res.append("null,");
+            }
+        }
+        res.deleteCharAt(res.length() - 1);
+        res.append("]");
+        return res.toString();
+    }
+
+    // Decodes your encoded data to tree.
+    public TreeNode deserialize(String data) {
+        if (data.equals("[]")) {
+            return null;
+        }
+        //划分出节点值
+        String[] vals = data.substring(1, data.length() - 1).split(",");
+        TreeNode root = new TreeNode(Integer.parseInt(vals[0]));
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+        int i = 1;
+        //按层构建二叉树
+        while (!queue.isEmpty()) {
+            TreeNode node = queue.poll();
+            if (!vals[i].equals("null")) {
+                node.left = new TreeNode(Integer.parseInt(vals[i]));
+                queue.add(node.left);
+            }
+            i++;
+            if (!vals[i].equals("null")) {
+                node.right = new TreeNode(Integer.parseInt(vals[i]));
+                queue.add(node.right);
+            }
+            i++;
+        }
+        return root;
+    }
+}
+
+// Your Codec object will be instantiated and called as such:
+// Codec codec = new Codec();
+// codec.deserialize(codec.serialize(root));
+//leetcode submit region end(Prohibit modification and deletion)
