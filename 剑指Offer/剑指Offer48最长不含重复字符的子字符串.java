0b602a9b9fbbc16b22d3e334d67d6c172ac96695
v <3496655347@qq.com>
2021-02-15 22:51:54 +0800

    6ms 动态规划+哈希表

---
+++ 剑指Offer/剑指Offer48最长不含重复字符的子字符串.java
@@ -0,0 +1,62 @@
+//请从字符串中找出一个最长的不包含重复字符的子字符串，计算该最长子字符串的长度。 
+//
+// 
+//
+// 示例 1: 
+//
+// 输入: "abcabcbb"
+//输出: 3 
+//解释: 因为无重复字符的最长子串是 "abc"，所以其长度为 3。
+// 
+//
+// 示例 2: 
+//
+// 输入: "bbbbb"
+//输出: 1
+//解释: 因为无重复字符的最长子串是 "b"，所以其长度为 1。
+// 
+//
+// 示例 3: 
+//
+// 输入: "pwwkew"
+//输出: 3
+//解释: 因为无重复字符的最长子串是 "wke"，所以其长度为 3。
+//     请注意，你的答案必须是 子串 的长度，"pwke" 是一个子序列，不是子串。
+// 
+//
+// 
+//
+// 提示： 
+//
+// 
+// s.length <= 40000 
+// 
+//
+// 注意：本题与主站 3 题相同：https://leetcode-cn.com/problems/longest-substring-without-rep
+//eating-characters/ 
+// Related Topics 哈希表 双指针 Sliding Window 
+// 👍 149 👎 0
+
+
+//leetcode submit region begin(Prohibit modification and deletion)
+class Solution {
+    public int lengthOfLongestSubstring(String s) {
+        Map<Character, Integer> dic = new HashMap<>();
+        int res = 0, tmp = 0;
+        for(int j = 0; j < s.length(); j++) {
+            //获取索引 i
+            int i = dic.getOrDefault(s.charAt(j), -1);
+
+            //更新哈希表
+            dic.put(s.charAt(j), j);
+
+            // dp[j - 1] -> dp[j]
+            tmp = tmp < j - i ? tmp + 1 : j - i;
+
+            // max(dp[j - 1], dp[j])
+            res = Math.max(res, tmp);
+        }
+        return res;
+    }
+}
+//leetcode submit region end(Prohibit modification and deletion)
