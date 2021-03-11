faa97117425645f912e9b660c5a58fdf0f04d06b
vigilr <3496655347@qq.com>
2021-03-11 22:31:58 +0800

    更新仓库

--- 数据库/[184]部门工资最高的员工.sql
+++
@@ -1,9 +0,0 @@
-SELECT d.name as Department,e.name as Employee,e.salary as Salary
-FROM department as d
-    join employee as e
-    on e.departmentid=d.id
-WHERE (e.departmentid,e.salary) in (
-SELECT departmentid,max(salary)
-FROM employee
-GROUP BY departmentid
-);
