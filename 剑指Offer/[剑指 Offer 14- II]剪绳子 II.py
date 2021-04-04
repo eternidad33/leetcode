c5886a7e9175d31d85823c867b7bd90a29ca7839
vigilr <3496655347@qq.com>
2021-04-04 19:52:33 +0800

    32ms 快速幂，python不需考虑大数越界

---
+++ 剑指Offer/[剑指 Offer 14- II]剪绳子 II.py
@@ -0,0 +1,72 @@
+# 给你一根长度为 n 的绳子，请把绳子剪成整数长度的 m 段（m、n都是整数，n>1并且m>1），
+# 每段绳子的长度记为 k[0],k[1]...k[m - 1]。
+# 请问 k[0]*k[1]*...*k[m - 1] 可能的最大乘积是多少？
+# 例如，当绳子的长度是8时，我们把它剪成长度分别为2、3、3的三段，此时得到的最大乘积是18。
+# 
+#  答案需要取模 1e9+7（1000000007），如计算初始结果为：1000000008，请返回 1。 
+# 
+#  
+# 
+#  示例 1： 
+# 
+#  输入: 2
+# 输出: 1
+# 解释: 2 = 1 + 1, 1 × 1 = 1 
+# 
+#  示例 2: 
+# 
+#  输入: 10
+# 输出: 36
+# 解释: 10 = 3 + 3 + 4, 3 × 3 × 4 = 36 
+# 
+#  
+# 
+#  提示： 
+# 
+#  
+#  2 <= n <= 1000 
+#  
+# 
+#  注意：本题与主站 343 题相同：https://leetcode-cn.com/problems/integer-break/ 
+#  Related Topics 数学 动态规划 
+#  👍 92 👎 0
+
+
+# leetcode submit region begin(Prohibit modification and deletion)
+
+
+class Solution:
+    def remainder(self, x, a, p):
+        """快速幂求余 (x^a)%p"""
+        rem = 1
+        while a > 0:
+            if a % 2:
+                # 偶数时返回False
+                rem = (rem * x) % p
+            x = x ** 2 % p
+            a //= 2
+        return rem
+
+    def cuttingRope(self, n: int) -> int:
+        # python不需考虑大数越界
+        if n <= 3:
+            return n - 1
+        a, b = divmod(n, 3)
+        p = 1000000007
+        # a, b = n // 3, n % 3
+
+        if b == 1:
+            return 3 ** (a - 1) * 4 % p
+        elif b == 0:
+            return 3 ** a % p
+        else:
+            return 3 ** a * 2 % p
+
+
+# leetcode submit region end(Prohibit modification and deletion)
+while True:
+    a = int(input("请输入a："))
+    if a % 2:
+        print("{}%2 为True".format(a))
+    else:
+        print("{}%2 为False".format(a))
