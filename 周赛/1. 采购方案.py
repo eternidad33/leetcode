df107935a4e8c8c39d85e751d4e68fb8aafe2646
vigilr <3496655347@qq.com>
2021-04-05 18:05:57 +0800

    ac

--- 周赛/1. 采购方案.py
+++ 周赛/1. 采购方案.py
@@ -3,11 +3,27 @@ from typing import List
 
 class Solution:
     def purchasePlans(self, nums: List[int], target: int) -> int:
-        if not nums or len(nums) == 0:
-            return 0
-        count = 0
-        for i in range(len(nums)):
-            for j in range(i + 1, len(nums)):
-                if nums[i] + nums[j] <= target:
-                    count += 1
-        return count
+        # # 超时
+        # if not nums or len(nums) == 0:
+        #     return 0
+        # count = 0
+        # for i in range(len(nums)):
+        #     for j in range(i + 1, len(nums)):
+        #         if nums[i] + nums[j] <= target:
+        #             count += 1
+        # return count
+
+        # 双指针
+        nums.sort()
+        i, j, res = 0, len(nums) - 1, 0
+        while i < j:
+            if nums[i] + nums[j] > target:
+                j -= 1
+            else:
+                res += (j - i)
+                i += 1
+        return res % 1000000007
+
+
+s = Solution()
+print(s.purchasePlans([2, 5, 3, 5], 6))
