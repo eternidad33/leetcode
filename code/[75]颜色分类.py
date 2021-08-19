ea7f82677230451a2b20c40d7870c242598a8007
vigilr <3496655347@qq.com>
2021-08-19 18:08:23 +0800

    update

--- code/[75]颜色分类.py
+++ code/[75]颜色分类.py
@@ -84,6 +84,17 @@ class Solution:
         #         nums[i] = 2
 
         # 方法三：双指针
-        # todo
+        n = len(nums)
+        p0, p1 = 0, 0
+        for i in range(n):
+            if nums[i] == 1:
+                nums[i], nums[p1] = nums[p1], nums[i]
+                p1 += 1
+            if nums[i] == 0:
+                nums[i], nums[p0] = nums[p0], nums[i]
+                if p0 < p1:
+                    nums[i], nums[p1] = nums[p1], nums[i]
+                p0 += 1
+                p1 += 1
 
 # leetcode submit region end(Prohibit modification and deletion)
