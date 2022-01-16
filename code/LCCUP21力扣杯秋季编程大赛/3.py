78ccece8b86fe4359fab30ca605f851f12cd07ad
tian <3496655347@qq.com>
2022-01-16 10:08:00 +0800

    delete code

--- code/LCCUP21力扣杯秋季编程大赛/3.py
+++
@@ -1,5 +0,0 @@
-from typing import List
-
-
-def flipChess(chessboard: List[str]) -> int:
-    """黑白棋翻转"""
--- code/数据结构基础/215数组中的第K个最大元素.py
+++
@@ -1,7 +0,0 @@
-from typing import List
-
-
-def findKthLargest(nums: List[int], k: int) -> int:
-    size = len(nums)
-    nums.sort()
-    return nums[size - k]
